297d896e478538f416bbbf8e95cef80accc58314
Shrey Parmar <107606723+ShreyParmar928@users.noreply.github.com>
2022-07-22 19:34:31 +0530

    Create SquarePattern.cpp

---
+++ Patterns 1/SquarePattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int N, i = 0;
+    cin >> N;
+    while (i < N){
+        int j = 0;
+        while (j < N){
+            cout << N;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
