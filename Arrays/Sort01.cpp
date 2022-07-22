1126f272b85a0cc3840c836bbd4dbce9e4f6594b
Shrey Parmar <107606723+ShreyParmar928@users.noreply.github.com>
2022-07-22 19:51:18 +0530

    Create Sort01.cpp

---
+++ Arrays/Sort01.cpp
@@ -0,0 +1,13 @@
+void sortZeroesAndOne(int arr[], int n){    
+     int count = 0; 
+    for (int i = 0; i < n; i++) { 
+        if (arr[i] == 0) 
+            count++; 
+    } 
+    for (int i = 0; i < count; i++){
+        arr[i] = 0; 
+    }
+    for (int i = count; i < n; i++) {
+        arr[i] = 1; 
+    }
+}
