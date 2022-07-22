60525b7b6d2c430908ccf1e28a087da22c0395f4
Shrey Parmar <107606723+ShreyParmar928@users.noreply.github.com>
2022-07-22 19:45:25 +0530

    Create SquareRoot(Integeral).cpp

---
+++ Operators and For Loop/SquareRoot(Integeral).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+    cin>>n;
+    int sq = 0;
+    
+    sq = sqrt(n);
+    cout<<sq;
+}
